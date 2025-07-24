947cc8fcc8a670c71d3c65a718421c671d808400
JJBcku <154745800+JJBcku@users.noreply.github.com>
2025-07-24 19:26:04 +0200

    Added bool64 enum

---
+++ Miscellaneous/Bool64.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Bool64Def.h"
+
+namespace Misc
+{
+	enum Bool64Values : Bool64
+	{
+		BOOL64_FALSE = 0,
+		BOOL64_TRUE = 0x5A5A5A5A5A5A5A5AUi64,
+	};
+}
---
+++ Miscellaneous/Bool64Def.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace Misc
+{
+	typedef uint64_t Bool64;
+	enum Bool64Values : Bool64;
+}
